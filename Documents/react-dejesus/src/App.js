117e6478c683d97aadb28e83ac45bb806ac83332
Daiana Dejesus <m.daiana.dejesus@gmail.com>
2022-08-24 19:19:45 -0300

    segunda entrega de react, agregue componentes y segemente el css de cada componente

---
+++ Documents/react-dejesus/src/App.js
@@ -0,0 +1,15 @@
+import NavBar from './Header/NavBar.js';
+import ItemListContainer from './Main/ItemListContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <ItemListContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Documents/react-dejesus/src/Main/ItemListContainer.js
@@ -0,0 +1,13 @@
+import '../Main/ItemListContainer.css'
+
+const ItemListContainer = () => {
+
+    return(
+        <section className='backgroundMain'>
+            <p className="texto">
+            Bienvenidx a mi primera app de React.
+            </p>    
+        </section>
+    )
+}
+export default ItemListContainer
